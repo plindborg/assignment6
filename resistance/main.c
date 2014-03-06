7a3928fad373701118e0aba295e4605e34c12df4
Patrick Lindborg <lindborg@gmail.com>
2014-03-06 22:21:53 +0000

    Some minor fixes and a non working Makefile, just a early draft

--- resistance/main.c
+++ resistance/main.c
@@ -6,7 +6,7 @@ int main(void)
 {
 	puts("Testing shared lib");
 	hello();
-	float array[3] = {1.1,1.1,1.3};
+	float array[3] = {1.1,1.1,0};
 	float x = calc_resistance(3,'P',array);
 	printf("%f",x);
 	return 0;
--- resistance/resistance.c
+++ resistance/resistance.c
@@ -2,15 +2,23 @@
 
 float calc_resistance(int count, char conn, float *array){
 	float total = 0.0;
-	for(int i = 0; i < count; i++) {
-		if(conn == 'P') {
-			if(array[i] = 0 )
-				return 0;	
-		}
-		printf("%f",array[i]);
-		total += array[i];
-	}
+	// Check so that conn is P or S.
+  if(conn != 'P' && conn != 'S') {
+	  return -1;
+  }
 
+  // Check so that array is not 0
+  if(array == 0) {
+    return -1;
+  }
+  
+  for(int i = 0; i < count; i++) {
+      if(conn == 'P') {
+			  if(array[i] == 0 )
+				  return 0;	
+		  }
+		  total += array[i];
+  }
 	return total;
 }
 
